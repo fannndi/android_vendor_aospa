92eddcfb8c9fae82dd647ce58181b0ad380fc22e
fannndi <trinity.x.dev@gmail.com>
2026-03-15 04:25:48 +0700

    Ota

--- build.sh
+++ build.sh
@@ -294,11 +294,11 @@ elif [ "$FLAG_IMG_ZIP" = 'y' ]; then
     checkExit
 
 else
-    m updatepackage "$CMD"
+    m otapackage "$CMD"
 
     checkExit
 
-    cp -f $OUT/aospa_$DEVICE-img.zip $OUT/aospa-$AOSPA_VERSION.zip
+    cp -f $OUT/aospa_$DEVICE-ota.zip $OUT/aospa-$AOSPA_VERSION.zip
     echo "Package Complete: $OUT/aospa-$AOSPA_VERSION.zip"
 fi
 echo -e ""
